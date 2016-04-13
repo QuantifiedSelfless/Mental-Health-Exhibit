cf13132f9a0c5bf5a3af2460752621ba6eeb2880
sako0938 <korn94sam@gmail.com>
2016-04-13 20:22:47 +0000

    added rfid

--- static/mental-health.js
+++ static/mental-health.js
@@ -24,6 +24,11 @@ var myDisplay;
 var qCount;
 var user_data;
 
+var socket = io.connect('http://localhost:3000')
+socket.on('rfid', function(data){
+    setTimeout(function() { window.location = "http://localhost:8000"}, 2000)
+})
+
 function preload() {
     game_data = loadJSON("questions.json");
 	phase0 = loadImage("static/phase0.png");
